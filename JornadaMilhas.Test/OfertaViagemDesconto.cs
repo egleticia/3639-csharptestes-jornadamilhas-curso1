b16b93781e5ce259fc571f6e7f276b516fc1e93e
Leticia Egle <leticiaegle@yahoo.com.br>
2024-05-28 22:56:17 -0300

    TDD

---
+++ JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,45 @@
+﻿
+
+using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualizado_QuandoAplicadoDesconto()
+        {
+            //ARRANGE
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 02, 01), new DateTime(2024, 02, 05));
+            double precoOriginal = 100.00;
+            double desconto = 20.00;
+            double precoComDesconto = precoOriginal - desconto;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            // ACT
+            oferta.Desconto = desconto;
+
+            //ASSERT
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+
+        [Fact]
+        public void RetornaPrecoCom70PorcentoDesconto_QuandoDescontoMaiorQuePreco()
+        {
+            //ARRANGE
+            Rota rota = new Rota("OrigemTeste", "DestinoTeste");
+            Periodo periodo = new Periodo(new DateTime(2024, 02, 01), new DateTime(2024, 02, 05));
+            double precoOriginal = 100.00;
+            double desconto = 120.00;
+            double precoComDesconto = 30;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            // ACT
+            oferta.Desconto = desconto;
+
+            //ASSERT
+            Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+        }
+    }
+}
